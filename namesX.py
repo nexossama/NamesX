69f6897578921ab537087170f5e0832c6cd85819
Ossama <you@example.com>
2022-06-07 02:36:47 +0200

    add extention choice option

--- namesX.py
+++ namesX.py
@@ -1,5 +1,5 @@
 import pathlib
-def NamesX(path,add_text="N",Frow=None):
+def NamesX(path,extention,add_text="N",Frow=None):
     p=pathlib.Path(path)
     for folder in p.iterdir():
         if folder.is_dir():
@@ -8,7 +8,7 @@ def NamesX(path,add_text="N",Frow=None):
             if add_text=="Y":
                 text_file.write(f"{Frow}\n")
             for file in folder.iterdir():
-                if file.suffix=="":
+                if file.suffix==extention:
                     text_file.write(f"{file.name}\n")
             text_file.close()
 
@@ -18,5 +18,6 @@ add_text=input("do you want to add a row ? (Y/N) ")
 Frow=None
 if add_text == 'Y':
     Frow = input("write first row text to add : ")
-    
-NamesX(rf"{path}",add_text,Frow)
+
+extention=input("enter file extention to select (ex .png) : ")
+NamesX(rf"{path}",extention,add_text,Frow)
