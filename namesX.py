d57883a77902e3de6297f22448163db9d59f97cc
Ossama <you@example.com>
2022-06-07 02:30:27 +0200

    add first row text option

--- namesX.py
+++ namesX.py
@@ -1,14 +1,22 @@
 import pathlib
-def namesX(path):
+def NamesX(path,add_text="N",Frow=None):
     p=pathlib.Path(path)
     for folder in p.iterdir():
         if folder.is_dir():
-            renameX(folder)
+            NamesX(folder)
             text_file=open(folder/"texts.txt","w")
+            if add_text=="Y":
+                text_file.write(f"{Frow}\n")
             for file in folder.iterdir():
-                if file.suffix==".png":
-                    text_file.write(f"{path}\n")
+                if file.suffix=="":
+                    text_file.write(f"{file.name}\n")
             text_file.close()
 
 path=input("input path : ")
-renameX(rf"{path}")
+
+add_text=input("do you want to add a row ? (Y/N) ")
+Frow=None
+if add_text == 'Y':
+    Frow = input("write first row text to add : ")
+    
+NamesX(rf"{path}",add_text,Frow)
